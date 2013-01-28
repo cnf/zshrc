18007e6e333ccf7cd7b5d00b7211e4ba655a1e61
Frank Rosquin <frank.rosquin@kangaroot.be>
2013-01-28 15:50:59 +0100

    better dir color

--- variables.zsh
+++ variables.zsh
@@ -12,9 +12,9 @@ WATCHFMT='%n %a %l from %m at %t.'
 export BROWSER='open'
 # http://geoff.greer.fm/lscolors/
 export CLICOLOR=1
-export LSCOLORS="ExGxFxDxCxDxDxhbhdacEc"
-export LSCOLORS="exfxcxdxbxegedabagacad"
-export LS_COLORS="di=34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:"
+# export LSCOLORS="ExGxFxDxCxDxDxhbhdacEc"
+export LSCOLORS="Exfxcxdxbxegedabagacad"
+export LS_COLORS="di=01;34:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:"
 export EDITOR='vim'
 export LESSOPEN="|lesspipe %s"
 export VISUAL=$EDITOR
