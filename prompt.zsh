aebfb16326b1f38b65c28384e16c471f09b89d20
Frank Rosquin <frank.rosquin@gmail.com>
2014-08-26 13:46:47 +0200

    added battery lvl indicator and did color code cleanup

--- prompt.zsh
+++ prompt.zsh
@@ -9,7 +9,7 @@ case $TERM in
             #vcs_info
             print -Pn "\e]0;%m — %2~\a"
         }
-        
+
         # Special function preexec, executed before running each command.
         function preexec () {
             print -Pn "\e]2;${1:q} — %2~\a"
@@ -20,24 +20,73 @@ esac
 autoload -z colors; colors
 
 if [ $UID -eq 0 ]; then
-    user="%{$fg_bold[red]%}%m%{$reset_color%}"
+    user="%B%F{red}%m%{$reset_color%}"
     symbol='#'
 else
-    user="%{$fg_bold[green]%}%m%{$reset_color%}"
+    user="%B%F{green}%m%{$reset_color%}"
     symbol='$'
 fi
 
+function battery_charge {
+    if [[ `uname` == 'Darwin' ]]
+    then
+        bat_data=`ioreg -rc AppleSmartBattery`
+        ac_adapt=`echo ${bat_data}|grep ExternalConnected| awk '{print $3}'`
+        charging=`echo ${bat_data}|grep IsCharging|awk '{print $3}'`
+        full=`echo ${bat_data}|grep FullyCharged|awk '{print $3}'`
+        bat_percent=`echo ${bat_data} | awk '$1~/Capacity/{c[$1]=$3} END{OFMT="%.0f"; max=c["\"MaxCapacity\""]; print (max>0? 100*c["\"CurrentCapacity\""]/max: "?")}'`
+    else
+        return
+    fi
+
+    if [[ ("$ac_adapt" == "Yes") && ("$full" == "Yes") ]]
+    then
+        bat_charge=""
+        bat_color=""
+        # return
+    elif [[ "$ac_adapt" == "Yes" && "$charging" == "Yes" ]]
+    then
+        bat_charge=`echo -e '\U26A1\UFE0E'`
+        bat_color="%B%F{black}"
+    else
+
+        if [[ $bat_percent -ge 80 ]]
+        then
+            bat_charge=`echo -e '\U0025CF\U0025CF\U0025CF'`
+            bat_color="%F{green}"
+        elif [[ $bat_percent -ge 60 ]]
+        then
+            bat_charge=`echo -e '\U0025CF\U0025CF\U0025CB'`
+            bat_color="%F{yellow}"
+        elif [[ $bat_percent -ge 40 ]]
+        then
+            bat_charge=`echo -e '\U0025CF\U0025CB\U0025CB'`
+            bat_color="%F{yellow}"
+        elif [[ $bat_percent -ge 20 ]]
+        then
+            bat_charge=`echo -e '\U0025CB\U0025CB\U0025CB'`
+            bat_color="%F{red}"
+        else
+            bat_charge=`echo -e '\U0025CB\U0025CB\U0025CB'`
+            bat_color="\e[5m%F{red}"
+        fi
+    fi
+    echo "%{$bat_color%}$bat_charge%{$reset_color%}"
+    # U+26A1⚡
+    return
+}
+
 function rsymbol {
-    git branch >/dev/null 2>/dev/null && echo "%{$fg_bold[yellow]%}±" && return
-    #hg root >/dev/null 2>/dev/null && echo "%{$fg_bold[yellow]%}☿" && return
-    [ -d .svn ] && echo "%{$fg_bold[yellow]%}⨰" && return
+    git branch >/dev/null 2>/dev/null && echo "%B%F{yellow}±" && return
+    #hg root >/dev/null 2>/dev/null && echo "%B%F{yellow}☿" && return
+    [ -d .svn ] && echo "%B%F{yellow]%}⨰" && return
     echo $symbol
 }
 
 function venv {
     [ $VIRTUAL_ENV ] || return
-    #echo "  %{$fg_bold[cyan]%}$(basename ${VIRTUAL_ENV} | tr '[A-Z]' '[a-z]')%{$reset_color%}"
-    echo "  %{$fg_bold[cyan]%}$(basename ${VIRTUAL_ENV})%{$reset_color%}"
+    # echo "  %B%F{cyan}$(basename ${VIRTUAL_ENV} | tr '[A-Z]' '[a-z]')%{$reset_color%}"
+    echo "  %B%F{cyan}$(basename ${VIRTUAL_ENV})%{$reset_color%}"
 }
 
 function getla {
@@ -59,7 +108,7 @@ function getla {
 #zstyle ':vcs_info:*' check-for-changes true
 #zstyle ':vcs_info:*' stagedstr "⚐"
 #zstyle ':vcs_info:*' unstagedstr "⚑"
-#zstyle ':vcs_info:git*' formats "%{$fg_bold[yellow]%}‹%r/%b› %m%u%c% %{$reset_color%}"
+#zstyle ':vcs_info:git*' formats "%B%F{yellow}‹%r/%b› %m%u%c% %{$reset_color%}"
 function vcs_prompt_info {
     echo "$(git_prompt_info)"
     #echo "${vcs_info_msg_0_}$(git_prompt_info)"
@@ -67,8 +116,8 @@ function vcs_prompt_info {
 
 
 setopt PROMPT_SUBST
-PROMPT='$user %{$fg_bold[blue]%}%2~ $(rsymbol) %{$reset_color%}'
-RPS1='$(vcs_prompt_info)%{$reset_color%}$(venv)  %{$fg_bold[black]%}$(getla)%{$reset_color%}'
-#RPS1='%{$reset_color%}$(venv)%{$reset_color%}  %{$fg_bold[black]%}$(getla)%{$reset_color%}'
+PROMPT='$user %B%F{blue}%2~ $(rsymbol) %{$reset_color%}'
+RPS1='$(vcs_prompt_info)%{$reset_color%}$(venv)  $(battery_charge)%{$reset_color%}'
+#RPS1='%{$reset_color%}$(venv)%{$reset_color%}  %B%F{black}$(getla)%{$reset_color%}'
 
 export VIRTUAL_ENV_DISABLE_PROMPT=1
